79ed2efa45180fa6282ccc48d479a3abbf856f53
Neeraj Dalal <admin@nrjdalal.com>
2025-01-28 03:02:39 +0530

    feat: enable theme toggle

--- src/components/social-dropdown.tsx
+++ src/components/social-dropdown.tsx
@@ -24,7 +24,7 @@ export default function SocialDropdown() {
             fill="currentColor"
             xmlns="http://www.w3.org/2000/svg"
             width="14"
-            height="14"
+            height="13"
           >
             <path d="M14 12.25H9.68L6.297 7.97l-3.873 4.28H.276L5.295 6.7 0 0h4.43l3.06 3.916L11.025 0h2.147L8.485 5.19 14 12.25Zm-3.727-1.244h1.189L3.783 1.18H2.507l7.766 9.827Z" />
           </svg>
